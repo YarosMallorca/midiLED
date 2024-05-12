4f3181822b8f0a9df5dd63e36315a4d5f334f297
Yaros <thedj.launchpadder.dmx512@gmail.com>
2024-05-12 23:50:50 +0200

    Added multiple led strip functionality

--- midiLED.ino
+++ midiLED.ino
@@ -1,42 +1,76 @@
 #include <Adafruit_NeoPixel.h>
 
-#define _numLED 128  // Number of LEDs in your underlights device
-#define pinLED  4    // DIN Pin
-#define pStart  36   // First note in array (36 is default, maps to C1 in Ableton)
-#define bLED    100   // Maximum Overall Brightness of LEDs (0-255) [Set to 255 if you have a power supply]
-#define rDelay  80   // Refresh Delay (lower = more FPS) if set too low will lag on high LED Counts
-#define ledsPerChannel 127 // Number of Leds controlled per one MIDI Channel
+// # ----------------------- #
+// #     GENERAL CONFIG      #
+// # ----------------------- #
+#define _numLED 128 // Number of LEDs in your underlights device
+#define pStart 36   // First note in array (36 is default, maps to C1 in Ableton)
+#define bLED 255    // Maximum Overall Brightness of LEDs (0-255) [Set to 255 if you have a power supply]
+#define rDelay 80   // Refresh Delay (lower = more FPS) if set too low will lag on high LED Counts
 
+// # --------------------- #
+// #     MIDI PALETTE      #
+// # --------------------- #
 const byte _r[128] = {0, 65, 130, 255, 255, 255, 130, 65, 255, 255, 130, 65, 255, 255, 130, 65, 134, 81, 40, 20, 73, 0, 0, 0, 73, 0, 0, 0, 73, 0, 0, 0, 73, 0, 0, 0, 73, 0, 0, 0, 73, 0, 0, 0, 45, 0, 0, 0, 105, 45, 24, 12, 255, 255, 130, 65, 255, 255, 130, 65, 255, 150, 117, 32, 0, 0, 0, 0, 0, 16, 125, 28, 255, 186, 174, 97, 12, 0, 0, 0, 24, 89, 174, 40, 255, 134, 113, 0, 57, 85, 53, 89, 49, 105, 210, 255, 255, 182, 142, 130, 57, 0, 12, 20, 20, 101, 130, 219, 215, 255, 158, 101, 20, 219, 125, 154, 142, 61, 113, 223, 158, 53, 24, 4, 182, 61, 178, 73};
 const byte _g[128] = {0, 65, 130, 255, 61, 0, 0, 0, 186, 61, 32, 16, 174, 255, 130, 65, 255, 255, 130, 65, 255, 255, 130, 65, 255, 255, 130, 65, 255, 255, 130, 65, 255, 255, 130, 65, 194, 166, 85, 45, 134, 85, 45, 24, 36, 0, 0, 0, 53, 0, 0, 0, 61, 0, 0, 0, 65, 0, 0, 0, 12, 53, 81, 53, 57, 73, 20, 0, 69, 0, 125, 28, 0, 255, 235, 255, 138, 255, 166, 40, 0, 0, 24, 16, 49, 223, 255, 255, 255, 255, 255, 138, 81, 81, 28, 0, 69, 166, 255, 89, 40, 73, 77, 20, 28, 57, 0, 65, 73, 190, 223, 178, 20, 210, 235, 150, 101, 61, 113, 255, 0, 0, 206, 65, 174, 49, 81, 20};
 const byte _b[128] = {0, 65, 130, 255, 61, 0, 0, 0, 105, 0, 0, 0, 45, 0, 0, 0, 49, 0, 0, 0, 73, 0, 0, 0, 93, 24, 12, 4, 89, 85, 45, 24, 182, 150, 73, 36, 255, 255, 130, 65, 255, 255, 130, 65, 255, 255, 130, 65, 251, 255, 130, 65, 255, 255, 130, 65, 109, 81, 40, 20, 0, 0, 0, 4, 0, 24, 109, 255, 77, 202, 125, 28, 0, 45, 4, 8, 0, 93, 255, 255, 255, 255, 121, 0, 0, 4, 20, 0, 36, 109, 202, 255, 194, 231, 255, 89, 0, 0, 0, 4, 0, 12, 32, 40, 89, 24, 0, 40, 16, 36, 45, 12, 45, 105, 138, 255, 255, 61, 113, 255, 0, 0, 0, 0, 0, 0, 0, 0};
 
-Adafruit_NeoPixel _LED = Adafruit_NeoPixel(_numLED, pinLED, NEO_GRB + NEO_KHZ800);
+// # ------------------------- #
+// #     LED STRIP CONFIG      #
+// # ------------------------- #
+#define numPins 4 // Number of LED Strips
+// ! WARNING ! This should match with the length of the _LED array
+Adafruit_NeoPixel _LED[numPins] = {
+    Adafruit_NeoPixel(_numLED, 3, NEO_GRB + NEO_KHZ800),
+    Adafruit_NeoPixel(_numLED, 4, NEO_GRB + NEO_KHZ800),
+    Adafruit_NeoPixel(_numLED, 5, NEO_GRB + NEO_KHZ800),
+    Adafruit_NeoPixel(_numLED, 6, NEO_GRB + NEO_KHZ800)};
 
 int count = 0;
 
-void noteOn(byte ch, byte p, byte v) {
-  _LED.setPixelColor((p + (ledsPerChannel * (ch-1))) - pStart, _r[v], _g[v], _b[v]);
-  count = 0;
+void noteOn(byte ch, byte p, byte v)
+{
+  int pin = ch - 1;
+  if (pin >= 0 && pin < numPins)
+  {
+    _LED[pin].setPixelColor(p - pStart, _r[v], _g[v], _b[v]);
+    count = 0;
+  }
 }
 
-void noteOff(byte ch, byte p, byte v) {
-  _LED.setPixelColor((p + (ledsPerChannel * (ch-1))) - pStart, 0, 0, 0);
-  count = 0;
+void noteOff(byte ch, byte p, byte v)
+{
+  int pin = ch - 1;
+  if (pin >= 0 && pin < numPins)
+  {
+    _LED[pin].setPixelColor(p - pStart, 0, 0, 0);
+    count = 0;
+  }
 }
 
-void setup() {
-  _LED.begin();
-  _LED.setBrightness(bLED);
+void setup()
+{
+  for (int i = 0; i < numPins; i++)
+  {
+    _LED[i].begin();
+    _LED[i].setBrightness(bLED);
+    _LED[i].show();
+  }
+
   usbMIDI.setHandleNoteOn(noteOn);
   usbMIDI.setHandleNoteOff(noteOff);
-  _LED.show();
 }
 
-void loop() {
-  while (usbMIDI.read());
+void loop()
+{
+  while (usbMIDI.read())
+    ;
   count++;
-  if (count == rDelay) {
-    _LED.show();
+  if (count == rDelay)
+  {
+    for (int i = 0; i < numPins; i++)
+    {
+      _LED[i].show();
+    }
   }
 }
